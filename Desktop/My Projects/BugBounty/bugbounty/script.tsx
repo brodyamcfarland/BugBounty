f9f6ce0d5b71dd35154f93b03d78c46c0f3e9c5e
brodyamcfarland <brodyamcfarland@gmail.com>
2022-08-22 18:05:37 -0700

    DB finished

--- Desktop/My Projects/BugBounty/bugbounty/script.tsx
+++ Desktop/My Projects/BugBounty/bugbounty/script.tsx
@@ -2,15 +2,28 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const main = async () => {
+// Creates a user
+export const createUser = async () => {
     prisma.user.create({ data: {email: 'brody@gmail.com', username: 'Off2Beans'}})
 }
 
-main()
-    .catch(e => {
-        console.error(e.message)
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    })
+// Deletes a user
+export const deleteUser = async () => {
+    prisma.user.delete({id: 0})
+}
+
+//Add a Post
+export const createPost = async () => {
+    prisma.post.create({ data: {
+        title: 'BLANK',
+        content: 'BLANK',
+        tags: ['solidity'],
+        price: 1
+    }})
+}
+
+//Delete a Post
+export const deletePost = async () => {
+    prisma.post.delete()
+}
 
